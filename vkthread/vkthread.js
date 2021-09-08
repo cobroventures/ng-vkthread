a5fbdc6b44a90c2df362af6f756668ea2617f45c
adeopura4 <adeopura@cobroventures.com>
2021-09-08 16:03:46 -0400

    bug/NGM-7848-web-worker-limit
    Limit on the number of web workers that can be present at any time

--- vkthread/vkthread.js
+++ vkthread/vkthread.js
@@ -51,7 +51,19 @@
      */
     var VkthreadProvider = function(){
 
-        this.$get = ['$q', '$timeout', function($q, $timeout) {
+        this.$get = ['$q', '$timeout', '$log', function($q, $timeout, $log) {
+                // This is the default number of simultaneous workers
+                // that we will have at any given time
+            var DEFAULT_NUM_MAX_THREADS = 2,
+                // This is the maximum number of simultaneous workers
+                // even if the user machine has more cores.
+                NUM_MAX_THREADS = 8,
+                // List of workers (busy and available)
+                workerPool = [],
+                // List of pending tasks
+                taskQueue = [],
+                // Number of effective maximum workers
+                numMaxActiveWorkers;
 
             var VkThread = function(){
                 this.version = '2.5.0';
@@ -60,32 +72,207 @@
                 };
             };
 
-          /**
-           *   Execute function in a thread.
-           *
-           *    @param -- object;
-           *
-           *    @param object has following attributes
-           *
-           *      @fn          - function to execute                (mandatory)
-           *      @args        - array of arguments for @fn          (optional)
-           *      @context     - object which will be 'this' for @fn (optional)
-           *      @importFiles - array of strings                    (optional)
-           *                     each string is a path to a file, which @fn depends on.
-           */
+            /**
+             * @private
+             * @function initializeNumMaxActiveWorkers
+             *
+             * @description Initialize the maximum  number of workers to have
+             * at any given time. We do not want to arbitrarily create a large
+             * number of workers since that leads to a crash.
+             *
+             * Further, a large number of workers results in a significant overhead,
+             * so we will limit it to a certain maximum value in all cases.
+             */
+            function initializeNumMaxActiveWorkers() {
+              // Get the number logical processors from the call below.
+              numMaxActiveWorkers = getNumLogicalProcessors();
+              if (!numMaxActiveWorkers) {
+                // The call above is not supported on certain browsers (safari for instance).
+                // What we do in that instance is to just assume a certain number of max allowed
+                // workders.
+                numMaxActiveWorkers = DEFAULT_NUM_MAX_THREADS;
+              }
+
+              // Limit the number of workers at any time to NUM_MAX_WORKERS.
+              numMaxActiveWorkers = Math.min(numMaxActiveWorkers, NUM_MAX_THREADS);
+            }
+
+            /**
+             * @function getNumLogicalProcessors
+             *
+             * @description Get the number of logical processors available to
+             * run threads on the user's computer. Note that this property is
+             * not supported on all browsers, so this value may be undefined on
+             * some browsers.
+             *
+             * @see
+             * https://developer.mozilla.org/en-US/docs/Web/API/Navigator/hardwareConcurrency
+             *
+             * @returns {Number}
+             */
+            function getNumLogicalProcessors() {
+              return navigator.hardwareConcurrency;
+            }
+
+            /**
+             * @private
+             * @function enqueueTask
+             *
+             * @description Enqueue the task, now it is in the pending queue
+             *
+             * @param {Object} param - Contains the function to execute in the web worker.
+             *
+             * @param {Object} deferred - The deferred to resolve or reject once the worker
+             * is done with the processing.
+             *
+             */
+            function enqueueTask(param, deferred) {
+              taskQueue.push({
+                param: param,
+                deferred: deferred
+              });
+            }
+
+            /**
+             * @private
+             * @function dequeueTask
+             *
+             * @description Dequeue the task at the first position
+             *
+             * @return {Object} contains param, deferred
+             */
+            function dequeueTask() {
+              return taskQueue.shift();
+            }
+
+            /**
+             * @private
+             * @function performNextTaskIfWorkerAvailable
+             *
+             * @description Perform the next task if there is a task pending and there is a
+             * worker available.
+             *
+             */
+            function performNextTaskIfWorkerAvailable() {
+              var availableWorker = getAvailableWorker();
+
+              if (!availableWorker) {
+                // There is no available worker, exit for now. Once a worker
+                // completes, it will pick up the next task from the task queue.
+                return;
+              }
+
+              // Dequeue task, if any
+              var taskInfo = dequeueTask();
+
+              if (!taskInfo) {
+                // There is no pending task, so there is nothing to do.
+                return;
+              }
+
+              // This will make the call to run the function in the web worker.
+              execTaskCore(availableWorker, taskInfo);
+            }
+
+            /**
+             * @private
+             * @function createWorker
+             *
+             * @description Create and return a web worker
+             *
+             * @return {Object} the worker that was created
+             */
+            function createWorker() {
+              var worker = new Worker(window.URL.createObjectURL(workerBlob));
+              return worker;
+            }
+
+            /**
+             * @private
+             * @function getAvailableWorker
+             *
+             * @description Get an available worker, if any. This function will
+             * create a worker if the limit is not yet reached.
+             *
+             * @return {Object} Contains worker and isAvailable fields
+             *
+             */
+            function getAvailableWorker() {
+              var availableWorker;
+
+              // Determine if there is any worker that has isAvailable set to true
+              var availableWorkers = workerPool.filter(function(workerInfo) {
+                return workerInfo.isAvailable
+              });
+
+              if (availableWorkers.length) {
+                // Available worker found, we can use that worker for the next task
+                availableWorker = availableWorkers[0];
+              } else if (workerPool.length < numMaxActiveWorkers) {
+                // There is no available worker, but can still create a new worker since the
+                // limit is not yet reached.
+                availableWorker = {
+                  worker: createWorker(),
+                  // Currently the worker is available to process work
+                  isAvailable: true
+                };
+
+                // Add the worker to the worker poll
+                workerPool.push(availableWorker);
+              }
+
+              return availableWorker;
+            }
+
+            /**
+             * @function exec
+             *
+             * @description Enqueue the task, now it is in the pending queue
+             *
+             * @param {Object} param - Contains the function to execute in the web worker.
+             *
+             */
             VkThread.prototype.exec = function(param){
-                var worker = new Worker(window.URL.createObjectURL(workerBlob)),
-                    dfr = $q.defer(),
+              var deferred = $q.defer();
+
+              // Enqueue the task
+              enqueueTask(param, deferred);
+
+              // Execute the task if there is a worker available
+              performNextTaskIfWorkerAvailable();
+
+              // Return the promise
+              return deferred.promise;
+            }
+
+            /**
+             * @private
+             * @function execTaskCore
+             *
+             * @description Execute the task on the web worker
+             *
+             * @param {Object} workerInfo - Contains the available worker
+             *
+             * @param {Object} taskInfo - Contains the param and deferred
+             *
+             * @return {Promise} Resolves to the data as computed after the execution
+             * of the function specified.
+             *
+             */
+            function execTaskCore(workerInfo, taskInfo){
+                var worker = workerInfo.worker,
+                    param = taskInfo.param,
+                    dfr = taskInfo.deferred,
                     workerTimer;
 
                 worker.onmessage = function (oEvent) {
                   // Use the data as per the result of the function that was executed
-                  handleWorkerCompletion(worker, workerTimer, dfr, oEvent.data);
+                  handleWorkerCompletion(workerInfo, workerTimer, dfr, oEvent.data);
                 };
 
                 worker.onerror = function(error) {
                   // Use the value as provided for the error case
-                  handleWorkerCompletion(worker, workerTimer, dfr, param.returnValueToUseUponError);
+                  handleWorkerCompletion(workerInfo, workerTimer, dfr, param.returnValueToUseUponError);
                 };
 
                 if (param.maxExecutionDuration && (param.maxExecutionDuration > 0)) {
@@ -97,9 +284,13 @@
                     // such as a regex leading to a very long execution time.
                     // Use the value as provided for the timeout case
                     workerTimer = null;
-                    handleWorkerCompletion(worker, workerTimer, dfr, param.returnValueToUseUponTimeout);
+                    handleWorkerCompletion(workerInfo, workerTimer, dfr, param.returnValueToUseUponTimeout);
                   }, param.maxExecutionDuration, false);
                 }
+
+                // Mark that the worker is no longer available since it is busy processing
+                // a task.
+                workerInfo.isAvailable = false;
 
                 worker.postMessage(JSONfn.stringify(param));
                 return dfr.promise;
@@ -107,15 +298,19 @@
 
             // This function is called when the worker completes (success or failure)
             // For simplicity, we will always resolve the promise
-            function handleWorkerCompletion(worker, workerTimer, dfr, data){
+            function handleWorkerCompletion(workerInfo, workerTimer, dfr, data){
               dfr.resolve(data);
 
               // Clear the timer
               clearTimer(workerTimer);
-              if (worker) {
-                // Terminate the worker
-                worker.terminate();
+              if (workerInfo) {
+                // The worker is done with the task, indicate that the worker is
+                // now available for another task.
+                workerInfo.isAvailable = true;
               }
+
+              // Attempt to execute the next task, if any.
+              performNextTaskIfWorkerAvailable();
             }
 
             function clearTimer(workerTimer) {
@@ -146,6 +341,9 @@
                   }
                 );
             };
+
+            // Initialize the maximum number of threads that we can create
+            initializeNumMaxActiveWorkers();
 
             var vkThread = function() {
                return new VkThread();
